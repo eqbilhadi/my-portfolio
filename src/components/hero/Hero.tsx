375770ec289cfc4695253423dc2e33fbaca26b37
eqtada bilhadi <muhamadeqtada@gmail.com>
2024-07-31 09:22:17 +0700

    Fix hero section

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -8,22 +8,24 @@ import HeroText from "./HeroText";
 
 export function Hero() {
   return (
-    <AuroraBackground>
-      <motion.div
-        initial={{ opacity: 0.0, y: 40 }}
-        whileInView={{ opacity: 1, y: 0 }}
-        transition={{
-          delay: 0.3,
-          duration: 0.8,
-          ease: "easeInOut",
-        }}
-        className="container mx-auto"
-      >
-        <div className="flex flex-col justify-between gap-x-8 dark:text-white xl:flex-row">
-          <HeroImage />
-          <HeroText />
-        </div>
-      </motion.div>
-    </AuroraBackground>
+    <section id="hero">
+      <AuroraBackground>
+        <motion.section
+          initial={{ opacity: 0.0, y: 40 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{
+            delay: 0.3,
+            duration: 0.8,
+            ease: "easeInOut",
+          }}
+          className="container mx-auto"
+        >
+          <div className="flex flex-col justify-between gap-x-8 dark:text-white xl:flex-row">
+            <HeroImage />
+            <HeroText />
+          </div>
+        </motion.section>
+      </AuroraBackground>
+    </section>
   );
 }
