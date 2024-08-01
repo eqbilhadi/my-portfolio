55e59e39b3ee9f82aa792d6b95bf7d9f3835946b
eqtada bilhadi <muhamadeqtada@gmail.com>
2024-08-01 11:24:07 +0700

    Update HeroText.tsx

--- src/components/hero/HeroText.tsx
+++ src/components/hero/HeroText.tsx
@@ -6,10 +6,10 @@ export default function HeroText() {
       <div className="text-sm uppercase font-semibold mb-4 tracking-[4px]">
         Web Developer
       </div>
-      <div className="h1">Hello, I'm</div>
+      <div className="h1">Hello, I&apos;m</div>
       <div className="h1 mb-4">Eqtada Bilhadi Muhamad</div>
       <p className="subtitle">
-        As a web developer I help businesses overcome digital challenges with innovative, responsive and scalable web solutions. Let's work together to make your vision a reality!
+        As a web developer I help businesses overcome digital challenges with innovative, responsive and scalable web solutions. Let&apos;s work together to make your vision a reality!
       </p>
     </div>
   );
