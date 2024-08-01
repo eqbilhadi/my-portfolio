a961b5f9c30b0510afba338859df05b825ade9d6
eqtada bilhadi <muhamadeqtada@gmail.com>
2024-08-01 11:48:17 +0700

    Fix ui

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -6,7 +6,7 @@ export default function Projects() {
   return (
     <section
       id="projects"
-      className="mx-auto flex w-[90%] flex-col items-center justify-center lg:max-w-[1212.8px] z-[20] pt-24"
+      className="mx-auto flex w-[90%] flex-col items-center justify-center lg:max-w-[1212.8px] z-[20] pt-24 pb-52"
     >
       <ProjectTitleAnimate />
       <div className="mb-24 grid w-full grid-cols-1 grid-rows-2 gap-x-6 gap-y-6 lg:max-w-[1200px] lg:grid-cols-2">
